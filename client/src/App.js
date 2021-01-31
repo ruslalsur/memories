4cf22cbddb2a21aebd3a95a7bc7cd28aeda09f54
rusla <suhrugen@gmail.com>
2021-02-01 00:13:54 +0700

    change env brauser

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import { MainPage } from './pages/MainPage';
-import { AuthPage } from './pages/AuthPage';
-import { RegPage } from './pages/RegPage';
-import { Navbar } from './components/Navbar';
-import { APP_NAME } from './config.js';
-import './App.css';
+import React from 'react'
+import { Container } from '@material-ui/core'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import { MainPage } from './pages/MainPage'
+import { AuthPage } from './pages/AuthPage'
+import { RegPage } from './pages/RegPage'
+import { Navbar } from './components/Navbar'
+import { APP_NAME } from './config.js'
+import './App.css'
 
 function App() {
   const history = createBrowserHistory()
 
   return (
-    
-    <Router history={history}>
-      <Navbar appName={APP_NAME} />
-
-      <Switch>
-        <Container maxWidth="md">
-          <Route path='/' exact>
-            <MainPage />
-          </Route>
-          <Route path='/auth'>
-            <AuthPage />
-          </Route>
-          <Route path='/reg'>
-            <RegPage />
-          </Route>
-        </Container>
-      </Switch>
-    </Router>
-  );
+    <div class='page'>
+      <Router history={history}>
+        <Navbar appName={APP_NAME} />
+        <Switch>
+          <Container maxWidth='md'>
+            <Route path='/' exact>
+              <MainPage />
+            </Route>
+            <Route path='/auth'>
+              <AuthPage />
+            </Route>
+            <Route path='/reg'>
+              <RegPage />
+            </Route>
+          </Container>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App
