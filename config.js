98e8c24d40b07e9dfc36f253ce695b28d697065b
rusla <suhrugen@gmail.com>
2021-02-16 01:15:59 +0700

    ch prod port

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   mongoUri:
     'mongodb+srv://rusla:0o9i8u7y@cluster0.c4yxa.mongodb.net/memories?retryWrites=true&w=majority',
-  port: process.env.PROD ? 80 : 5000,
+  port: process.env.PROD ? 8000 : 5000,
   frontBuildPath: path.join(__dirname, 'client', 'build'),
   jwtSecret: 'R604658ANsfD78OM',
 }
