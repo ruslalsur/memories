8c4543a443ff832879682054ba291aed7dfd048d
rusla <suhrugen@gmail.com>
2021-03-23 06:59:57 +0700

    change filename of images

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 export const MEM_PER_PAGE = 9
 export const IMAGES_PATH = '/images/'
-export const NO_IMAGE = 'noimage.png'
-export const NO_AVATAR = 'noavatar.png'
+export const NO_IMAGE = 'noimage.jpg'
+export const NO_AVATAR = 'noavatar.jpg'
 export const RND_MEMORY_INTERVAL = 7000
