2dff567c4424115c024e815da3ad595d403fbb46
rusla <suhrugen@gmail.com>
2021-04-15 02:17:52 +0700

    increase random timer value

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 export const MEM_PER_PAGE = 9
 export const IMAGES_PATH = '/images/'
-export const RND_MEMORY_INTERVAL = 7000
+export const RND_MEMORY_INTERVAL = 15000
 export const LOCALSTORAGE_NAME = 'authorizedUserData'
 export const UPLOAD_FILE_SIZE = 1048576
 export const UPLOAD_FILE_SIZE_VIP = 3145728
